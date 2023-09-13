9f9e6cd05dd04e7165e8167dd210e7b53f3a821c
emberogl <embernebulan@gmail.com>
2023-09-13 09:19:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SortingHat
+{
+    internal class SortingHat
+    {
+        // Enum representing Hogwarts houses
+        public enum Houses
+        {
+            Gryffindor,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random random = new Random();
+                // Casting random integer to enum type to retrieve value, argument 2 creates array of enum values and gets length
+                Console.WriteLine($"You belong to {(Houses)random.Next(0, Enum.GetValues(typeof(Houses)).Length)}!");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
